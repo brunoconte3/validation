f718fec4dd5a926a900e579281c1af5a2fd1bd71
Dhonata <dhonatafreitas@gmail.com>
2017-07-20 22:40:31 -0300

    Atualizando Funções

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -7,72 +7,78 @@ class Validator {
     
     
     private $erros = false;
-
     
-    public function __construct(array $datas, array $rules){
-        foreach($rules as $ruleKey => $ruleValue){
-            if(isset($datas[$ruleKey])){
-                $this->rules($datas[$ruleKey], $ruleKey, $ruleValue);
+    private $data, $rules;
+
+    public function set(array $data, array $rules){
+        $this->data  = $data;
+        $this->rules = $rules; 
+    }
+
+    public function validate(){
+        foreach($this->rules as $ruleKey => $ruleValue){
+            if(isset($this->data[$ruleKey])){
+                $this->rules($ruleKey, $ruleValue);
             }
         }
     }
     
     
-    private function rules($data, $ruleKey, $ruleValue){
+    private function rules($ruleKey, $ruleValue){
         $conditions = explode('|', $ruleValue);
         
         foreach($conditions as $condition){
-            $this->validate($condition, $data, $ruleKey);
+            $this->run($condition, $ruleKey);
         }
     }
     
     
-    private function validate($condition, $data, $ruleKey){
+    private function run($condition, $ruleKey){
         $subitem = explode(':', $condition);
         
         switch($subitem[0]){
             case 'required':
-                if(empty($data) || $data == '' || $data == ' '){
+                if(empty($this->data[$ruleKey]) || $this->data[$ruleKey] == '' || $this->data[$ruleKey] == ' '){
                     $this->erros[] = "O campo {$ruleKey} é obrigatório.";
                 }
             break;
             case 'max':
-                if(strlen($data) > $subitem[1]){
+                if(strlen($this->data[$ruleKey]) > $subitem[1]){
                     $this->erros[] = "O campo {$ruleKey} precisa conter no máximo {$subitem[1]} caracteres.";
                 }
             break;
             case 'min':
-                if(strlen($data) < $subitem[1]){
+                if(strlen($this->data[$ruleKey]) < $subitem[1]){
                     $this->erros[] = "O campo {$ruleKey} precisa conter no mínimo {$subitem[1]} caracteres.";
                 }
             break;
             case 'email':
-                if(!filter_var($data, FILTER_VALIDATE_EMAIL)){
+                if(!filter_var($this->data[$ruleKey], FILTER_VALIDATE_EMAIL)){
                     $this->erros[] = "O campo {$ruleKey} é necessário que seja um email válido.";
                 }
             break;
             case 'url':
-                if(!filter_var($data, FILTER_VALIDATE_URL)){
+                if(!filter_var($this->data[$ruleKey], FILTER_VALIDATE_URL)){
                     $this->erros[] = "O campo {$ruleKey} é necessário que seja uma URL válido.";
                 }
             break;
             case 'numeric':
-                if(!is_numeric($data)){
+                if(!is_numeric($this->data[$ruleKey])){
                     $this->erros[] = "O campo {$ruleKey} só pode conter valores numéricos.";
                 }
             break;
             case 'float':
-                if(!filter_var($data, FILTER_VALIDATE_FLOAT)){
+                if(!filter_var($this->data[$ruleKey], FILTER_VALIDATE_FLOAT)){
                     $this->erros[] = "O campo {$ruleKey} deve ser do tipo real.";
                 }
             break;
             case 'int':
-                if(!filter_var($data, FILTER_VALIDATE_INT)){
+                if(!filter_var($this->data[$ruleKey], FILTER_VALIDATE_INT)){
                     $this->erros[] = "O campo {$ruleKey} deve ser do tipo inteiro.";
                 }
             break;
             case 'regex':
-                if(preg_match($subitem[1], $data) !== FALSE){
+                if(preg_match($subitem[1], $this->data[$ruleKey]) !== FALSE){
                     $this->erros[] = "O campo {$ruleKey} deve corresponder com as expecificações requisitadas.";
                 }
             break;
