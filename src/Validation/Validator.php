dbb44a4e7114130eec7e83c77e70e17e418566c3
Maurício Porfírio <mauriciosporfirio@gmail.com>
2017-07-24 20:41:20 -0300

    Atualizando validação

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -68,63 +68,63 @@ class Validator {
     
     
     private function validate($condition, $data, $ruleKey) {
-        $itens   = explode('@', $condition);
-        $subitem = explode(':', $itens[0]);
+        $message = explode('@', $condition);
+        $item    = explode(':', $message[0]);
         
-        switch($subitem[0]):
+        switch($item[0]):
             case 'required':
                 if(empty($data) || $data == '' || $data == ' '){
-                    $this->erros["$ruleKey"] = $itens[1] ?? "O campo $ruleKey é obrigatório.";
+                    $this->erros["$ruleKey"] = $message[1] ?? "O campo $ruleKey é obrigatório.";
                 }
             break;
             case 'max':
-                if(strlen($data) > $subitem[1]){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey precisa conter no máximo $subitem[1] caracteres.";
+                if(strlen($data) > $item[1]){
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey precisa conter no máximo $item[1] caracteres.";
                 }
             break;
             case 'min':
-                if(strlen($data) < $subitem[1]){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey precisa conter no mínimo $subitem[1] caracteres.";
+                if(strlen($data) < $item[1]){
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey precisa conter no mínimo $item[1] caracteres.";
                 }
             break;
             case 'bool':
                 if(!filter_var($data, FILTER_VALIDATE_BOOLEAN)){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey só pode conter valores lógicos. (true|false, 1|0, yes|no).";
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey só pode conter valores lógicos. (true|false, 1|0, yes|no).";
                 }
             break;
             case 'email':
                 if(!filter_var($data, FILTER_VALIDATE_EMAIL)){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey é necessário que seja um email válido.";
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey é necessário que seja um email válido.";
                 }
             break;
             case 'float':
                 if(!filter_var($data, FILTER_VALIDATE_FLOAT)){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey deve ser do tipo real.";
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey deve ser do tipo real.";
                 }
             break;
             case 'int':
                 if(!filter_var($data, FILTER_VALIDATE_INT)){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey deve ser do tipo inteiro.";
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey deve ser do tipo inteiro.";
                 }
             break;
             case 'ip':
                 if(!filter_var($data, FILTER_VALIDATE_IP)){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey deve ser um IP válido.";
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey deve ser um IP válido.";
                 }
             break;
              case 'mac':
                 if(!filter_var($data, FILTER_VALIDATE_MAC)){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey deve ser um MAC válido.";
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey deve ser um MAC válido.";
                 }
             break;
             case 'regex':
-                if(!preg_match($subitem[1], $data) !== FALSE){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey deve corresponder com as expecificações requisitadas.";
+                if(!preg_match($item[1], $data) !== FALSE){
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey deve corresponder com as expecificações requisitadas.";
                 }
             break;
             case 'url':
                 if(!filter_var($data, FILTER_VALIDATE_URL)){
-                    $this->erros["$ruleKey"] = $itens[1] ??  "O campo $ruleKey é necessário que seja uma URL válida.";
+                    $this->erros["$ruleKey"] = $message[1] ??  "O campo $ruleKey é necessário que seja uma URL válida.";
                 }
             break;
         endswitch;
