0086d1b4df5821386f21126335fbb353b12bd539
Maurício Porfírio <mauriciosporfirio@gmail.com>
2017-07-30 00:02:41 -0300

    Adicionando novas linguagens

--- languages/en.php
+++ languages/en.php
@@ -1,6 +1,6 @@
 <?php
 
-// Inglês|English
+// Inglês | English
 
 return [
     
---
+++ languages/fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Francês | Français
+
+return [
+    
+    'required' => 'Le champ {$field} est requis.',
+    'max'      => 'Le champ {$field} doit contenir un maximum de {$value} caractères.',
+    'min'      => 'Le champ {$field} doit contenir au moins {$value} caractères.',
+    'bool'     => 'Le champ {$field} ne peut contenir que des valeurs logiques. (true|false, 1|0, yes|no).',
+    'email'    => 'Le champ {$field} doit être une adresse email valide.',
+    'float'    => 'Le champ {$field} doit être de type réel (flottant).',
+    'int'      => 'Le champ {$field} doit être de type entier.',
+    'ip'       => 'Le champ {$field} doit être une adresse IP valide.',
+    'mac'      => 'Le champ {$field} doit être une adresse MAC valide.',
+    'regex'    => 'Le champ {$field} doit contenir une valeur avec un format valide.',
+    'url'      => 'Le champ {$field} doit être une adresse URL valide.'
+    
+];
+
