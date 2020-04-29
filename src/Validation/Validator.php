8e6d9de4cb201d79ff21fcb0880ae38892b53270
Bruno Conte <brunoconte3@gmail.com>
2020-04-29 20:18:20 -0300

    Alguns ajustes

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -1,179 +1,129 @@
 <?php
 
-namespace ValidationArray\Validation;
+namespace brunoconte3\Validation;
 
 class Validator
 {
-    /**
-     * @var type
-     */
-
-
     private $erros = false;
-
-
-    /**
-     * -------------------------------------------------------------------------
-     * Setando/Definindo regras para determinados dados.
-     * -------------------------------------------------------------------------
-     *
-     * @param array $datas
-     * @param array $rules
-     */
-
 
     public function set(array $datas, array $rules)
     {
-        foreach ($rules as $rule_key => $rule_value) {
-            if (isset($datas[$rule_key])) {
-                $this->rules($datas[$rule_key], $rule_key, $rule_value);
+        foreach ($rules as $ruleKey => $ruleValue) {
+            if (isset($datas[$ruleKey])) {
+                $this->rules($datas[$ruleKey], $ruleKey, $ruleValue);
             }
         }
     }
 
-
-    /**
-     * -------------------------------------------------------------------------
-     * Definindo mais de uma regra para um determinado dado.
-     * -------------------------------------------------------------------------
-     *
-     * @param type $data_value
-     * @param type $rule_key
-     * @param type $rule_value
-     */
-
-
-    private function rules($data_value, $rule_key, $rule_value)
+    private function rules($dataValue, $ruleKey, $ruleValue)
     {
-        $conditions = explode('|', $rule_value);
+        $conditions = explode('|', $ruleValue);
 
         foreach ($conditions as $condition) {
-            if (!isset($this->erros[$rule_key])) {
-                $this->validate($condition, $data_value, $rule_key);
+            if (!isset($this->erros[$ruleKey])) {
+                $this->validate($condition, $dataValue, $ruleKey);
             }
         }
     }
 
-
-    /**
-     * -------------------------------------------------------------------------
-     * Regras de validação para os dados.
-     * -------------------------------------------------------------------------
-     *
-     * @param type $condition
-     * @param type $data_value
-     * @param type $rule_key
-     */
-
-
-    private function validate($condition, $data_value, $rule_key)
+    private function validate($condition, $dataValue, $ruleKey)
     {
         $message = explode(',', $condition);
         $item    = explode(':', $message[0]);
 
         switch ($item[0]) {
             case 'required':
-                if (empty($data_value) || $data_value == '' || $data_value == ' ') {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key é obrigatório.";
+                if (empty($dataValue) || $dataValue == '' || $dataValue == ' ') {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey é obrigatório!";
                 }
                 break;
             case 'max':
-                if (strlen($data_value) > $item[1]) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key precisa conter no máximo $item[1] caracteres.";
+                if (strlen($dataValue) > $item[1]) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey precisa conter no máximo $item[1] caracteres!";
                 }
                 break;
             case 'min':
-                if (strlen($data_value) < $item[1]) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key precisa conter no mínimo $item[1] caracteres.";
+                if (strlen($dataValue) < $item[1]) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey precisa conter no mínimo $item[1] caracteres!";
                 }
                 break;
             case 'alpha':
-                if (!preg_match('/^([a-zÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖßÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ\s])+$/', $data_value) !== false) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key só pode conter caracteres alfabéticos.";
+                if (!preg_match('/^([a-zÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖßÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ\s])+$/', $dataValue) !== false) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey só pode conter caracteres alfabéticos!";
                 }
                 break;
             case 'alnum':
-                if (!preg_match('/^([a-z0-9ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖßÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ\s])+$/', $data_value) !== false) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve conter caracteres alfanuméricos.";
+                if (!preg_match('/^([a-z0-9ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖßÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ\s])+$/', $dataValue) !== false) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve conter caracteres alfanuméricos!";
                 }
                 break;
             case 'bool':
-                if (!filter_var($data_value, FILTER_VALIDATE_BOOLEAN)) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key só pode conter valores lógicos. (true|false, 1|0, yes|no).";
+                if (!filter_var($dataValue, FILTER_VALIDATE_BOOLEAN)) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey só pode conter valores lógicos. (true|false, 1|0, yes|no)!";
                 }
                 break;
             case 'email':
-                if (!filter_var($data_value, FILTER_VALIDATE_EMAIL)) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve ser um endereço de email válido.";
+                if (!filter_var($dataValue, FILTER_VALIDATE_EMAIL)) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve ser um endereço de email válido!";
                 }
                 break;
             case 'float':
-                if (!filter_var($data_value, FILTER_VALIDATE_FLOAT)) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve ser do tipo real(flutuante).";
+                if (!filter_var($dataValue, FILTER_VALIDATE_FLOAT)) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve ser do tipo real(flutuante)!";
                 }
                 break;
             case 'identifier':
-                if (!preg_match('/^([0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2})+$/', $data_value) !== false) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve corresponder ao formato 000.000.000-00.";
+                if (!preg_match('/^([0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2})+$/', $dataValue) !== false) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve corresponder ao formato 000.000.000-00!";
                 }
                 break;
             case 'int':
-                if (!filter_var($data_value, FILTER_VALIDATE_INT)) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve ser do tipo inteiro.";
+                if (!filter_var($dataValue, FILTER_VALIDATE_INT)) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve ser do tipo inteiro!";
                 }
                 break;
             case 'ip':
-                if (!filter_var($data_value, FILTER_VALIDATE_IP)) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve ser um endereço de IP válido.";
+                if (!filter_var($dataValue, FILTER_VALIDATE_IP)) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve ser um endereço de IP válido!";
                 }
                 break;
             case 'mac':
-                if (!filter_var($data_value, FILTER_VALIDATE_MAC)) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve ser um endereço de MAC válido.";
+                if (!filter_var($dataValue, FILTER_VALIDATE_MAC)) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve ser um endereço de MAC válido!";
                 }
                 break;
             case 'numeric':
-                if (!is_numeric($data_value)) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key só pode conter valores numéricos.";
+                if (!is_numeric($dataValue)) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey só pode conter valores numéricos!";
                 }
                 break;
             case 'phone':
-                if (!preg_match('/^(\([0-9]{2}\)[0-9]{4}-[0-9]{4})+$/', $data_value) !== false) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve corresponder ao formato (00)0000-0000.";
+                if (!preg_match('/^(\([0-9]{2}\)[0-9]{4}-[0-9]{4})+$/', $dataValue) !== false) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve corresponder ao formato (00)0000-0000!";
                 }
                 break;
             case 'plate':
-                if (!preg_match('/^[A-Z]{3}-[0-9]{4}+$/', $data_value) !== false) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve corresponder ao formato AAA-0000.";
+                if (!preg_match('/^[A-Z]{3}-[0-9]{4}+$/', $dataValue) !== false) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve corresponder ao formato AAA-0000!";
                 }
                 break;
             case 'regex':
-                if (!preg_match($item[1], $data_value) !== false) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key precisa conter um valor com formato válido.";
+                if (!preg_match($item[1], $dataValue) !== false) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey precisa conter um valor com formato válido!";
                 }
                 break;
             case 'url':
-                if (!filter_var($data_value, FILTER_VALIDATE_URL)) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve ser um endereço de URL válida.";
+                if (!filter_var($dataValue, FILTER_VALIDATE_URL)) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve ser um endereço de URL válida!";
                 }
                 break;
             case 'zip_code':
-                if (!preg_match('/^([0-9]{2}\.[0-9]{3}-[0-9]{3})+$/', $data_value) !== false) {
-                    $this->erros[$rule_key] = $message[1] ?? "O campo $rule_key deve corresponder ao formato 00.000-000";
+                if (!preg_match('/^([0-9]{2}\.[0-9]{3}-[0-9]{3})+$/', $dataValue) !== false) {
+                    $this->erros[$ruleKey] = $message[1] ?? "O campo $ruleKey deve corresponder ao formato 00.000-000";
                 }
                 break;
         }
     }
-
-
-    /**
-     * -------------------------------------------------------------------------
-     * Retorna todos os erros possíveis.
-     * -------------------------------------------------------------------------
-     *
-     * @return type
-     */
-
 
     public function getErros()
     {
