b7b438ad3a9d55af6fb27c9a11599c552072ed1b
Bruno Conte <brunoconte3@gmail.com>
2020-05-07 07:38:38 -0300

    Inclusão de mais testes

--- test/Test.php
+++ test/Test.php
@@ -6,9 +6,11 @@ use brunoconte3\Validation\Validator;
 
 $datas = [
     'sexo' => '',
+    'cnpj' => '33452731000159',
     'cnpjComMascara' => '33.452.731/0001-59',
     'telefone' => '44999696',
     'cpf' => '12547845874',
+    'cpfComMascara' => '125.478.458-74',
     'nome' => 'a',
     'numero' => 12345678,
     'email' => 'bruno.com',
@@ -16,13 +18,22 @@ $datas = [
     'validarPassandoJson' => '@&451',
     'idade' => 'a',
     'senha' => '@11111111',
+    'cep' => '8704750',
+    'data' => '31/04/1990',
+    'hora' => '24:03',
+    'url' => 'ww.test.c',
+    'ip' => '1.1.1',
+    'mac' => '00:00',
 ];
 
+//Aceita divisao das regras por PIPE ou formato JSON
 $rules = [
     'sexo' => 'required',
+    'cnpj' => 'required|min:18|max:18|companyIdentification',
     'cnpjComMascara' => 'required|min:18|max:18|companyIdentificationMask',
     'telefone' => 'required|phone',
     'cpf' => 'required|identifier',
+    'cpfComMascara' => 'required|identifierMask',
     'nome' => 'required|min:2',
     'numero' => 'max:5',
     'email' => 'email',
@@ -30,6 +41,12 @@ $rules = [
     'validarPassandoJson' => '{"required":"true","type":"alpha"}',
     'idade' => 'numeric',
     'senha' => 'float, O campo senha deve ser do tipo Inteiro!|required|max:8',
+    'cep' => '{"type":"zipcode"}',
+    'data' => 'dateBrazil',
+    'hora' => '{"type":"hour"}',
+    'url' => 'url',
+    'ip' => 'ip',
+    'mac' => 'mac',
 ];
 
 $validator = new Validator();
