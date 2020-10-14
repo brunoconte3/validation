ca7d04288c53ba9e646eee55f45c91e50841161e
Robson Willian <robsonwillian.ads@gmail.com>
2020-10-14 11:46:51 -0300

    Teste unitário UnitTestCompare.php

---
+++ Test/UnitTestCompare.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace brunoconte3\Test;
+
+use brunoconte3\Validation\Compare;
+use PHPUnit\Framework\TestCase;
+
+class UnitTestCompare extends TestCase
+{
+    public function testDaysDifferenceBetweenData(): void
+    {
+        $this->assertEquals('+30', Compare::daysDifferenceBetweenData('31/10/2020', '30/11/2020'));
+        $this->assertEquals('-19', Compare::daysDifferenceBetweenData('14/10/2020', '25/09/2020'));
+    }
+
+    public function testStartDateLessThanEnd(): void
+    {
+        $this->assertEquals(false, Compare::startDateLessThanEnd('30/11/2020', '31/10/2020'));
+        $this->assertEquals(true, Compare::startDateLessThanEnd('31/10/2020', '30/11/2020'));
+    }
+
+    public function testStartHourLessThanEnd(): void
+    {
+        $msg = 'Hora Inicial não pode ser maior que a Hora Final!';
+        $msgVazio = 'Um ou mais campos horas não foram preenchidos!';
+        $this->assertEquals($msg, Compare::startHourLessThanEnd('12:05:01', '10:20:01', $msg));
+        $this->assertEquals($msgVazio, Compare::startHourLessThanEnd('', '10:20:01', $msgVazio));
+        $this->assertEquals(null, Compare::startHourLessThanEnd('10:05:01', '12:20:01', $msg));
+    }
+
+    public function testCalculateAgeInYears(): void
+    {
+        $this->assertEquals('31', Compare::calculateAgeInYears('20/05/1989'));
+        $this->assertEquals('0', Compare::calculateAgeInYears('03/05/2020'));
+    }
+
+    public function testDifferenceBetweenHours(): void
+    {
+        $this->assertEquals('01:36:28', Compare::differenceBetweenHours('10:41:55', '12:18:23'));
+        $this->assertEquals('-12:59:55', Compare::differenceBetweenHours('23:00:05', '12:00:00'));
+    }
+
+    public function testCheckDataEquality(): void
+    {
+        $this->assertEquals(false, Compare::checkDataEquality('AçaFrão', 'Açafrão'));
+        $this->assertEquals(true, Compare::checkDataEquality('AçaFrão', 'Açafrão', false));
+    }
+}
