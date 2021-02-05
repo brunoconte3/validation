0ae16d7f48c0be5e33fb8bea7c69b1a3698abe56
Robson Willian <robsonwillian.ads@gmail.com>
2021-02-05 08:36:27 -0300

    Alteração no validateFile.php

--- src/Validation/ValidateFile.php
+++ src/Validation/ValidateFile.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace brunoconte3\Validation;
 
-use PhpParser\Node\Stmt\Break_;
-
 class ValidateFile
 {
     private static function validateFileTransformSingleToMultiple(array &$file = []): void
